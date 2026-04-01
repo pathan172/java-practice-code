c67e84375c1543ba4c8960b5ece118e635803eb2
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-01 19:25:39 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,12 @@
+public class basic {
+    public static void main(String[] args) {
+        String name = "Anzar";
+        String neighbor = "Ali";
+        int age = 25;
+        String friend = neighbor;
+        System.out.println(name);
+        System.out.println(neighbor);
+        System.out.println(age);
+        System.out.println(friend);
+    }
+}
