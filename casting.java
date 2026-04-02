01835a44c306c60d47625e2ac9fdf3eecfe4464b
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-02 23:55:15 +0530

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,12 @@
+public class casting {
+    public static void main(String[] args) {
+        // double price = 19.99;
+        // double finalPrice = price + 5.00;
+        // System.out.println(finalPrice);
+        // int price = 100;
+        // int finalPrice = price + (int) 18.00;
+        // System.out.println(finalPrice);
+        final float pi = 3.14f;
+        System.out.println(pi);
+    }
+}
