1360f75c6a29e793ba0a97236cd2784ba7a5b851
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-05 12:58:32 +0530

    Add files via upload
    Basic understanding of variables in java where we use the integer where the value is in integer and some basic terms of variables

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        double c = 3.14;
+        char d = 'A';
+        String name = "anzar";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(name);
+
+    }
+}
