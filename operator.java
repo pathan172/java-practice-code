1bdf57df4709a938c406a368ead6f5a9827882a9
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-06 22:34:04 +0530

    Add files via upload
    its a simple understanding of operator where its just like a math in we take a operator add,sub,mul,div.

---
+++ operator.java
@@ -0,0 +1,14 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 22;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("sum: " + sum);
+        System.out.println("sub: " + sub);
+        System.out.println("mul: " + mul);
+        System.out.println("div: " + div);
+    }
+}
