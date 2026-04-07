9e8b7bee373160b4f32b52d60d929c138cdf0b46
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-07 23:34:37 +0530

    Add files via upload
    its an operator of java in which i describe about comparison operator and  logical operator how to take in java using vs code

---
+++ operator2.java
@@ -0,0 +1,26 @@
+public class operator2 {
+    public static void main(String[] args) {
+        // comparison operator
+        // a ==b (true)
+        // a !=b (false)
+        // a > b (false)
+        // a < b (true)
+        // conditional operator
+        boolean isSunup = true;
+        if (isSunup == true) {
+            System.out.println("day");
+        } else {
+            System.out.println("night");
+
+        }
+        int age = 30;
+        if (age > 18){
+            System.out.println("can vote");
+        }
+        else{
+            System.out.println("cannot vote");
+        }
+
+    }
+
+}
