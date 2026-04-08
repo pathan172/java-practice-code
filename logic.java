041ccaeb7cdc63465d167a506f378aa9b19c6a47
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-08 23:46:13 +0530

    Add files via upload
    its a basic logic operator in java where we use the if else condition and there is a &&  operation use...

---
+++ logic.java
@@ -0,0 +1,13 @@
+public class logic {
+    public static void main(String[]args){
+        int a = 30;
+        int b = 40;
+        if (a<50 && b<50){
+            System.out.println("both are less than 50");;
+        }
+        else{
+            System.out.println("one of them is greater than 50");
+        }
+    }
+    
+}
