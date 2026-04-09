775f0fa1ec0051b4780426238bec402e8a834942
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-09 20:21:56 +0530

    Add files via upload
    its just a simple code in which we find the area of circle where we declare the r variable and area of circle which 3.14 *r*r...

---
+++ areaofcircle.java
@@ -0,0 +1,8 @@
+public class areaofcircle {
+    public static void main(String[] args) {
+        double r = 4.3;
+        double A = 3.14 * r * r;
+        System.out.println("Area of the circle is: " + A);
+    }
+
+}
