c294bb85626af4a9bbde97bc6ec2db1e37602d65
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-04-09 20:23:31 +0530

    Add files via upload
    its a simple code of java were we write a code of volume of sphere where we take a r in double and take a formula of sphere.....

---
+++ volume.java
@@ -0,0 +1,8 @@
+public class volume {
+    public static void main(String[] args) {
+        double r = 3.5;
+        double v = 4.0 / 3.0 * 3.14 * r * r * r;
+        System.out.println("Volume of the sphere is: " + v);
+    }
+
+}
